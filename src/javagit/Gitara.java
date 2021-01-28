95b81dd0370daa2981ebefbba667be22987f2de1
zaru <markoruza1@gmail.com>
2021-01-29 00:34:05 +0100

    gitara objekt

---
+++ src/javagit/Gitara.java
@@ -0,0 +1,22 @@
+package javagit;
+
+public class Gitara {
+	private int id;
+	private String brand;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	
+}
